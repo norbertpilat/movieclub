0a7c0008b540a8d77a1b961734225aafe248f27d
Norbert <norbx@MacBook-Pro-Norbert.local>
2023-09-28 21:31:08 +0200

    Initial commit

---
+++ src/test/java/lnp/movieclub/MovieclubApplicationTests.java
@@ -0,0 +1,13 @@
+package lnp.movieclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieclubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
